2d46f97380cdd7e9d1efd035923f08f4a19d6b8f
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-08-28 13:40:04 +0300

    fix app.css

--- frontend/src/features/Edit/StudentEdit.jsx
+++ frontend/src/features/Edit/StudentEdit.jsx
@@ -1,41 +1,41 @@
-import React from "react";
-import { useState, useEffect } from "react";
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 
-function StudentEdit ({student}) {
-    const [thanks, setThanks] = useState(0)
+function StudentEdit({ student }) {
+  const [thanks, setThanks] = useState(0);
 
-  
-  
-    
-    function povtor() {
-      setThanks(() => {
+  function povtor() {
+    setThanks(() => {
       fetch(`/phaseshift/${student.id}`)
         .then((result) => result.json())
         .then((data) => {
           setThanks(data.status);
         });
     }, []);
-
   }
 
-    return (
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div>
-                <button id={student.id} onClick={povtor} style={{listStyle: 'none', border: '1px solid black', margin: '5px', width: '500px', height: '50px' }} key={student.id}>
-                {student.name}  
-                <div>{thanks}</div>
-                </button>
-            </div>
-            <div>
-                {/* <button className="plusButton" onClick={plus}>+</button>
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div>
+        <button
+          id={student.id}
+          onClick={povtor}
+          key={student.id}
+        >
+          {student.name}
+          <div>{thanks}</div>
+        </button>
+      </div>
+      <div>
+        {/* <button className="plusButton" onClick={plus}>+</button>
                 <button className="minusButton" onClick={minus}>-</button> */}
-            </div>
-            {/* <div>
+      </div>
+      {/* <div>
                 {thanks}
             </div>  */}
-        </div>
-    )
+    </div>
+  );
 }
-
 
 export default StudentEdit;
