f3e0f876732c737f22dc59e88ac3640745745672
ElijahPavlov <capelinx@gmail.com>
2022-08-28 16:04:43 +0300

    login added

--- frontend/src/features/Edit/StudentEdit.jsx
+++ frontend/src/features/Edit/StudentEdit.jsx
@@ -15,17 +15,16 @@ function StudentEdit({ student }) {
     }, []);
   }
 
-
-    return (
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div>
-                <button id={student.id} onClick={povtor} style={{listStyle: 'none', border: '1px solid black', margin: '5px', width: '500px', height: '50px' }} key={student.id}>
-                {student.name}  
-                <div>{thanks}</div>
-                </button>
-            </div>
-        </div>
-    )
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div>
+        <button id={student.id} onClick={povtor} style={{ listStyle: 'none', border: '1px solid black', margin: '5px', width: '500px', height: '50px' }} key={student.id}>
+          {student.name}
+          <div>{thanks}</div>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default StudentEdit;
