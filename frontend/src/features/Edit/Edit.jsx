0b7314dfc0d2bab5cd9217b0b15b5a879e863405
ElijahPavlov <capelinx@gmail.com>
2022-08-28 21:14:23 +0300

    styles

--- frontend/src/features/Edit/Edit.jsx
+++ frontend/src/features/Edit/Edit.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-useless-fragment */
 /* eslint-disable no-undef */
 /* eslint-disable max-len */
 /* eslint-disable react/button-has-type */
@@ -8,21 +9,19 @@ function Edit() {
   const [students, setStudents] = useState([]);
   const [phase, setPhase] = useState(3);
 
-
- // Отрисовывает студентов согласно фазе в состоянии
-useEffect(() => {
+  // Отрисовывает студентов согласно фазе в состоянии
+  useEffect(() => {
     fetch(`/phaseshift/phase/${phase}`)
       .then((result) => result.json())
       .then((data) => setStudents(data));
   }, [phase]);
 
-
   // Изменяет состояние фазы
   function nextPhase() {
-    if(phase > 1) {
-      setPhase(phase-1);
+    if (phase > 1) {
+      setPhase(phase - 1);
     }
-  };
+  }
 
   // Перевод студентов на следующие фазы с учетом повторов и удаление 3 фазы
   async function shiftPhase() {
@@ -33,54 +32,50 @@ useEffect(() => {
 
     console.log('result', result);
     setStudents(result);
-    setPhase(phase-1);
-  };
+    setPhase(phase - 1);
+  }
 
   return (
     <div>
-    <div className="App">
-    <header className="App-header">
-    {(phase === 0)? 
-      <>
-      <form >
-        <h1>Добавить нового студента:</h1>
-      <input type="text" name="name" placeholder='name of new student' />
-      <br/>
-      <input type="text" name="phase" placeholder='phase of new student' />
-      <br/>
-      <input type="text" name="thanks" placeholder='thanks of new student' />
-      <br/>
-      <br/>
-        <button>Добавить студента</button>
-      </form>
-      <form type="submit" action="/list">
-        <button>Перейти к списку студентов</button>
-      </form>
-      </>
-      :
-      <></>
-  }
-        {(phase >= 1)?
-      <h1>{`Выберете повторщиков c фазы ${phase}`}</h1>
-      :
-      <></>
-        }
-      <ul>
-          {students.map((student) => 
-              <StudentEdit student={student}/>
-          )}
-      </ul>
-      {(phase > 1)?
-        <button onClick={nextPhase}>{`Перейти к фазе ${phase-1}`}</button>
-        :
-        <>
-        {(phase === 1)?
-          <button onClick={shiftPhase}> Добавить новых студентов</button>
-          :
-          <></>
-        }
-        </>
-      }
+      <div className="App">
+        <header className="App-header">
+          {(phase === 0)
+            ? (
+              <>
+                <form>
+                  <h1>Добавить нового студента:</h1>
+                  <input type="text" name="name" placeholder="name of new student" />
+                  <br />
+                  <input type="text" name="phase" placeholder="phase of new student" />
+                  <br />
+                  <input type="text" name="thanks" placeholder="thanks of new student" />
+                  <br />
+                  <br />
+                  <button>Добавить студента</button>
+                </form>
+                <form type="submit" action="/list">
+                  <button>Перейти к списку студентов</button>
+                </form>
+              </>
+            )
+            : <></>}
+          {(phase >= 1)
+            ? <h1>{`Выберете повторщиков c фазы ${phase}`}</h1>
+            : <></>}
+          <ul>
+            {students.map((student) =>
+              <StudentEdit student={student} />
+            )}
+          </ul>
+          {(phase > 1)
+            ? <button onClick={nextPhase}>{`Перейти к фазе ${phase - 1}`}</button>
+            : (
+              <>
+                {(phase === 1)
+                  ? <button onClick={shiftPhase}> Добавить новых студентов</button>
+                  : <></>}
+              </>
+            )}
 
         </header>
       </div>
