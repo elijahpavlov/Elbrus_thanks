b1daa52628b18771d85e896faa86f07e942653cc
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-08-29 15:22:48 +0300

    add lk-page

--- frontend/src/features/App.jsx
+++ frontend/src/features/App.jsx
@@ -1,16 +1,29 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/react-in-jsx-scope */
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import Edit from './Edit/Edit';
 import List from './List/List';
 import Main from './Main/Main';
+import Lk from './Lk/Lk';
 
 function App() {
+  // useEffect(() => {
+  //   async function getUser() {
+  //     const response = await fetch('/');
+  //     const result = await response.json();
+  //     console.log(result);
+  //   }
+  //   getUser();
+  // }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/list" element={<List />} />
       <Route path="/edit" element={<Edit />} />
+      <Route path="/lk" element={<Lk />} />
     </Routes>
   );
 }
