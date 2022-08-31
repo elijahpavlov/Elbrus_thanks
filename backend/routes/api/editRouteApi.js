537231684231b3f5a4ed69dae338c450f98c7400
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-08-31 09:18:27 +0300

    add edit router

---
+++ backend/routes/api/editRouteApi.js
@@ -0,0 +1,27 @@
+const editRouter = require('express').Router();
+const { Student } = require('../../db/models');
+
+editRouter.put('/:id', async (req, res) => {
+  try {
+    const student = await Student.findByPk(Number(req.params.id));
+
+    console.log(student);
+
+    if (!student) {
+      res.status(404).json({ success: false, message: 'Нет такого студента' });
+      return;
+    }
+
+    if (req.body.status === 'changeStudent') {
+      student.name = req.body.name;
+      student.phase = req.body.phase;
+    }
+
+    await student.save();
+    res.json(student);
+  } catch (error) {
+    res.redirect('/error');
+  }
+});
+
+module.exports = editRouter;
